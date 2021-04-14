58cdd57d505850bc6ebedaeddb95f3b8aaac6b14
Lucia <2313970630@qq.com>
2021-04-14 21:44:07 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Lucia/>
+</template>
+
+<script>
+import Lucia from './components/Lucia.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lucia
+  }
+}
+</script>
