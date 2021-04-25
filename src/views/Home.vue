c19f5c0e2f65b9f6dacab7f2f34814ddd6be6259
Lucia <2313970630@qq.com>
2021-04-25 22:46:02 +0800

    Home页面的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Topnav/>
+    <Topnav />
     
     <div class="banner">
         <h1>UI框架</h1>
@@ -13,7 +13,7 @@
   </div>
 </template>
 <script lang='ts'>
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue"
 export default {
   components:{
     Topnav
@@ -23,4 +23,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
