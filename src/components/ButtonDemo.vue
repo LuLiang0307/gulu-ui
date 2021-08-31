bfabdf75bbda455fd70475bdfbfa7f50bd6b9101
Lucia <2313970630@qq.com>
2021-08-31 17:26:54 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <Button>你好</Button>
+    <Button @click="clickButton" @focus="clickButton" @mouseover="clickButton" size="small">你好</Button>
   </div>
 </template>
 <script lang='ts'>
 import Button from '../lib/Button.vue';
   export default{
-    components:{Button}
+    components:{Button},
+    setup(){
+      const clickButton = ()=>{
+        console.log("hi")
+      }
+      return {clickButton}
+    }
   }
   
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,11 +1,19 @@
 <template>
-  <button>
-    <slot/>
-  </button>
+    <div :size="size">
+        <button v-bind="rest">
+            <slot/>
+        </button>
+    </div>
 </template>
-<script scoped>
-  
+<script lang='ts'>
+    export default{
+        inheritAttrs:false,//禁止继承父组件的属性
+        setup(props, context){
+            const {size,...rest} = context.attrs;
+            return {size, rest}
+        }
+    }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
      
 </style>
