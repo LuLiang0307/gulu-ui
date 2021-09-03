21ecbf7e22a56b24593f964d9cad5f55057ec64a
Lucia <2313970630@qq.com>
2021-09-03 17:30:23 +0800

    渲染Tabs内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    tabs示例
-  </div>
+  <div>tabs示例</div>
+  <Tabs>
+    <Tab title="导航一">Tab1</Tab>
+    <Tab title="导航二">Tab2</Tab>
+  </Tabs>
 </template>
 <script>
-  
-  
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+};
 </script>
 <style module lang="scss">
-     
 </style>
