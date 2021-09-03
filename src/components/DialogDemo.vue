be8908fdf4774bcd3611cab2f78cb6eca4065a57
Lucia <2313970630@qq.com>
2021-09-03 15:23:35 +0800

    Dialog 支持自定义 title 和 content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,30 +2,39 @@
   <div>Dialog示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visiable="x" :closeOnClickOverlay="false"
-    :ok="f1" :cancel="f2"
-  ></Dialog>
+  <Dialog
+    v-model:visiable="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  >
+    <template v-slot:content>
+      <strong>Hi</strong>
+      <div>Hi2</div>
+    </template>
+    <template v-slot:title> 
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
 </template>
 <script lang='ts'>
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
-import { ref } from 'vue';
+import { ref } from "vue";
 export default {
-  components:{Dialog,Button},
-  setup(){
-    const x = ref(false)
-    const toggle = ()=>{
-      x.value = !x.value
-    }
-    const f1=()=>{
-      return false
-    }
-    const f2=()=>{
-    }
-    return {x,toggle,f1,f2}
-  }
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
 };
 </script>
 <style lang="scss">
-
 </style>
