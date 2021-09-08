1f3e4cac9e0c8232bb61d967d1d12ed3b4eeb459
Lucia <2313970630@qq.com>
2021-09-09 00:07:55 +0800

    改个高亮css主题

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -34,7 +34,7 @@ import Button from "../lib/Button.vue";
 import Switch1Demo from "../components/Switch1.demo.vue";
 import Switch2Demo from "../components/Switch2.demo.vue";
 import 'prismjs';
-import 'prismjs/themes/prism.css'
+import 'prismjs/themes/prism-okaidia.css'
 const Prism = window.Prism;
 export default {
   components: {
