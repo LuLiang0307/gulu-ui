bffe534e38a3375f47c8c8991ce015d841d968e0
Lucia <2313970630@qq.com>
2021-09-09 16:42:51 +0800

    改造Button

---
+++ src/components/Button4.demo.vue
@@ -0,0 +1,26 @@
+<demo>
+支持 disabled
+</demo>
+<template>
+ <h1>示例4</h1>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+</template>
+<script lang='ts'>
+import Button from "../lib/Button.vue";
+
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
+};
+</script>
+<style scoped lang="scss">
+</style>
---
+++ src/components/Button5.demo.vue
@@ -0,0 +1,25 @@
+<demo>
+支持显示加载中
+</demo>
+<template>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
+</template>
+<script lang='ts'>
+import Button from "../lib/Button.vue";
+
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
+};
+</script>
+<style scoped lang="scss">
+</style>
