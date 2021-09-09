89f578b2d3b8a8f2d7d15ba7e13dee92fd530097
Lucia <2313970630@qq.com>
2021-09-09 16:19:09 +0800

    显示Demo标题

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,30 +1,44 @@
 <template>
-  <Demo :component="Switch1Demo"/>
-  <Demo :component="Switch2Demo"/>
+  <div class="demo">
+    <h2>{{ component.__sourceCodeTitle }}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre
+        class="language-html"
+        v-html="
+          Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')
+        "
+      />
+    </div>
+  </div>
 </template>
-<script>
-import { ref } from "vue";
-import Switch from "../lib/Switch.vue";
+
+<script lang="ts">
 import Button from "../lib/Button.vue";
-import Switch1Demo from "../components/Switch1.demo.vue";
-import Switch2Demo from "../components/Switch2.demo.vue";
 import "prismjs";
-import "prismjs/themes/prism-okaidia.css";
-import Demo from "../components/Demo.vue";
-const Prism = window.Prism;
+import "prismjs/themes/prism.css";
+const Prism = (window as any).Prism;
 export default {
   components: {
-    Switch,
     Button,
-    Demo,
+  },
+  props: {
+    component: Object,
   },
   setup() {
-    const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo, Prism };
+    return {
+      Prism,
+    };
   },
 };
 </script>
-<style scoped lang="scss">
+
+<style lang="scss" scoped>
 $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
